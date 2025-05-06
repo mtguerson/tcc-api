70f449d90d4e18c3cea0d526e264f35442038219
Reshzera <reoshiro13@gmail.com>
2025-05-05 20:11:27 -0400

    fix: change to deepseek to use the tool from groq

--- scripts/trigger.ts
+++ scripts/trigger.ts
@@ -1,55 +1,57 @@
-import { Pool } from "pg";
+import { Pool } from 'pg'
 
-const args = process.argv.slice(2);
-const isDrop = args[0] === "drop";
+const args = process.argv.slice(2)
+const isDrop = args[0] === 'drop'
 
 export abstract class Trigger {
-  pool: Pool;
-  functionName: string;
-  triggerName: string;
-  tableName: string;
+  pool: Pool
+  functionName: string
+  triggerName: string
+  tableName: string
 
   constructor(
     pool: Pool,
     tableName: string,
     functionName: string,
-    triggerName: string
+    triggerName: string,
   ) {
-    this.pool = pool;
-    this.tableName = tableName;
-    this.functionName = functionName;
-    this.triggerName = triggerName;
+    this.pool = pool
+    this.tableName = tableName
+    this.functionName = functionName
+    this.triggerName = triggerName
   }
 
   async create() {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
+
   async drop() {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.')
   }
 
   successMessage() {
     if (isDrop) {
       console.log(
-        `Function ${this.functionName} and trigger ${this.triggerName} dropped successfully. ✅`
-      );
-      return;
+        `Function ${this.functionName} and trigger ${this.triggerName} dropped successfully. ✅`,
+      )
+      return
     }
     console.log(
-      `Function ${this.functionName} and trigger ${this.triggerName} created successfully. ✅`
-    );
+      `Function ${this.functionName} and trigger ${this.triggerName} created successfully. ✅`,
+    )
   }
+
   errorMessage(error: any) {
-    console.error(error);
+    console.error(error)
     if (isDrop) {
       console.log(
-        `Error dropping function ${this.functionName} and trigger ${this.triggerName}. ❌`
-      );
+        `Error dropping function ${this.functionName} and trigger ${this.triggerName}. ❌`,
+      )
 
-      return;
+      return
     }
     console.log(
-      `Error creating function ${this.functionName} and trigger ${this.triggerName}. ❌`
-    );
+      `Error creating function ${this.functionName} and trigger ${this.triggerName}. ❌`,
+    )
   }
 }
